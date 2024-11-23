1508eb2256a046f960f851abcbfa2aa57c81a1c8
Azzaya Bayarsaikhan <azzayabayarsaikhan21@gmail.com>
2024-11-23 21:02:57 +0800

    initial

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, redirect
+import mysql.connector
+
+app = Flask(__name__)
+
+# MySQL холболтын тохиргоо
+DB_CONFIG = {
+    'host': '127.0.0.1',
+    'user': 'root',
+    'password': 'Lunalapin21',
+    'database': 'team_project',
+    'ssl_disabled': True 
+}
+
+# Нүүр хуудас: Мэдээллийг харах
+@app.route('/')
+def index():
+    conn = mysql.connector.connect(**DB_CONFIG)
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM students")  # Өөрийн хүснэгтийг оруулна
+    data = cursor.fetchall()
+    conn.close()
+    return render_template('./index.html', data=data)
+
+# Мэдээлэл нэмэх
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        conn = mysql.connector.connect(**DB_CONFIG)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO students (name, age) VALUES (%s, %s)", (name, age))
+        conn.commit()
+        conn.close()
+        return redirect('/')
+    return render_template('add.html')
+
+# Мэдээлэл засах
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    conn = mysql.connector.connect(**DB_CONFIG)
+    cursor = conn.cursor(dictionary=True)
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        cursor.execute("UPDATE students SET name = %s, age = %s WHERE id = %s", (name, age, id))
+        conn.commit()
+        conn.close()
+        return redirect('/')
+    cursor.execute("SELECT * FROM students WHERE id = %s", (id,))
+    record = cursor.fetchone()
+    conn.close()
+    return render_template('edit.html', record=record)
+
+# Мэдээлэл устгах
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete(id):
+    conn = mysql.connector.connect(**DB_CONFIG)
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM students WHERE id = %s", (id,))
+    conn.commit()
+    conn.close()
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
